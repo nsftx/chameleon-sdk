cbbe3cdcb0ead06f9b58563e8aa777763822d970
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-03-29 17:41:24 +0200

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'import/extensions': ['error', 'always', {
       'js': 'never',
     }],
+    'import/no-extraneous-dependencies': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-underscore-dangle': ['error', {
