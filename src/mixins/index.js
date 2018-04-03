c1c118a19680dc2c928c6666231263a2660998dd
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-04-03 11:45:28 +0200

    Add bindable to mixin list

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,3 +1,4 @@
+export { default as bindable } from './bindable';
 export { default as dependable } from './dependable';
 export { default as elementable } from './elementable';
 export { default as sourceable } from './sourceable';
