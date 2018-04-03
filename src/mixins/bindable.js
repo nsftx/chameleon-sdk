e1cc7b4368402d6574af1fa9185f133dbb9b6988
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-04-03 12:12:18 +0200

    Return reference with reduce

--- src/mixins/bindable.js
+++ src/mixins/bindable.js
@@ -1,11 +1,12 @@
-import { get, startsWith } from 'lodash';
+import { startsWith } from 'lodash';
 
 export default {
   methods: {
     getBindingValue(value) {
-      if (this.registry.bindableElements && startsWith(value, '=')) {
-        const bindingValue = value.substring(1);
-        return get(this.registry.bindableElements, bindingValue);
+      const elements = this.registry.bindableElements;
+      if (elements && startsWith(value, '=')) {
+        const binding = value.substring(1);
+        return binding.split('.').reduce((o, i) => o[i], elements);
       }
 
       return value;
