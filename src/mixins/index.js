a6641d062e6376693ae2b2b4492bdd660d816b96
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-04-04 16:08:59 +0200

    Fix typo

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -2,3 +2,4 @@ export { default as bindable } from './bindable';
 export { default as dependable } from './dependable';
 export { default as elementable } from './elementable';
 export { default as sourceable } from './sourceable';
+export { default as reactionable } from './reactionable';
