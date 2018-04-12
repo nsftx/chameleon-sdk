72c9a599442e1f5fc86e6c6e6dfd29bf0100b672
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-04-12 15:58:19 +0200

    Fix typo

--- src/mixins/reactionable.js
+++ src/mixins/reactionable.js
@@ -48,7 +48,7 @@ export default {
       action: 'actionName',
       schema: [{
         name: "name",
-        "mapName", "mapName",
+        mapName, "mapName",
       }],
       data: {
         key: value,
