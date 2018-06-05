db36aeea5fca808fa523a895f33f0433e6e68237
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-06-05 15:46:52 +0200

    Add simple worpress test #9

--- src/api/connector/external/wordpress.spec.js
+++ src/api/connector/external/wordpress.spec.js
@@ -1,4 +1,15 @@
+import axiosMock from 'axios';
+import postsMock from '../../../../test/__data__/posts.json';
 import wordpress from './wordpress';
+
+const connectorMock = {
+  name: 'wordpress',
+  type: 'rest',
+  disabled: false,
+  options: {
+    endpoint: null,
+  },
+};
 
 describe('wordpress connector', () => {
   it('should have methods exposed', () => {
@@ -6,5 +17,20 @@ describe('wordpress connector', () => {
     expect(wordpress.getSourceSchema).toBeTruthy();
     expect(wordpress.getSourceData).toBeTruthy();
     expect(wordpress.changeSourceData).toBeTruthy();
+  });
+
+  it('should get POSTS data', (done) => {
+    axiosMock.get.mockImplementation(() => Promise.resolve({
+      data: postsMock,
+    }));
+
+    const options = {};
+
+    wordpress.getSourceData(connectorMock, {
+      name: 'posts',
+    }, options).then((result) => {
+      expect(result).toEqual(postsMock);
+      done();
+    });
   });
 });
