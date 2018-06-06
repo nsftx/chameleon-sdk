efb0107c685230b7ec9a369e2e704b9e5767c678
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-06-06 09:47:58 +0200

    Add additional tests #9

--- src/api/connector/external/wordpress.spec.js
+++ src/api/connector/external/wordpress.spec.js
@@ -1,5 +1,8 @@
+/* eslint import/no-unresolved:"off" */
 import axiosMock from 'axios';
-import postsMock from '../../../../test/__data__/posts.json';
+import sourcesMock from 'data/wordpress-sources.json';
+import postsSchemaMock from 'data/wordpress-posts-schema.json';
+import postsMock from 'data/wordpress-posts.json';
 import wordpress from './wordpress';
 
 const connectorMock = {
@@ -13,19 +16,43 @@ const connectorMock = {
 
 describe('wordpress connector', () => {
   it('should have methods exposed', () => {
-    expect(wordpress.getSources).toBeTruthy();
-    expect(wordpress.getSourceSchema).toBeTruthy();
-    expect(wordpress.getSourceData).toBeTruthy();
-    expect(wordpress.changeSourceData).toBeTruthy();
+    const compareType = 'function';
+    expect(typeof wordpress.getSources).toEqual(compareType);
+    expect(typeof wordpress.getSourceSchema).toEqual(compareType);
+    expect(typeof wordpress.getSourceData).toEqual(compareType);
+    expect(typeof wordpress.changeSourceData).toEqual(compareType);
   });
 
-  it('should get POSTS data', (done) => {
+  it('should get sources', (done) => {
+    axiosMock.get.mockImplementation(() => Promise.resolve({
+      data: sourcesMock,
+    }));
+
+    const options = {};
+    wordpress.getSources(connectorMock, options).then((result) => {
+      expect(result).toEqual(sourcesMock.sources);
+      done();
+    });
+  });
+
+  it('should get posts schema', (done) => {
+    axiosMock.get.mockImplementation(() => Promise.resolve({
+      data: postsSchemaMock,
+    }));
+
+    const options = {};
+    wordpress.getSourceSchema(connectorMock, options).then((result) => {
+      expect(result).toEqual(postsSchemaMock);
+      done();
+    });
+  });
+
+  it('should get posts data', (done) => {
     axiosMock.get.mockImplementation(() => Promise.resolve({
       data: postsMock,
     }));
 
     const options = {};
-
     wordpress.getSourceData(connectorMock, {
       name: 'posts',
     }, options).then((result) => {
---
+++ src/api/connector/internal/graphql.spec.js
@@ -0,0 +1,34 @@
+/* eslint import/no-unresolved:"off" */
+import axiosMock from 'axios';
+import sourcesMock from 'data/graphql-sources.json';
+import graphql from './graphql';
+
+const connectorMock = {
+  name: 'chameleon',
+  type: 'graphql',
+  disabled: false,
+  options: {
+    endpoint: null,
+  },
+};
+
+describe('graphql connector', () => {
+  it('should have methods exposed', () => {
+    const compareType = 'function';
+    expect(typeof graphql.getSources).toEqual(compareType);
+    expect(typeof graphql.getSourceSchema).toEqual(compareType);
+    expect(typeof graphql.getSourceData).toEqual(compareType);
+  });
+
+  it('should get sources', (done) => {
+    axiosMock.post.mockImplementation(() => Promise.resolve({
+      data: sourcesMock,
+    }));
+
+    const options = {};
+    graphql.getSources(connectorMock, options).then((result) => {
+      expect(result.noYesChoices.model).toEqual('NoYesChoice');
+      done();
+    });
+  });
+});
