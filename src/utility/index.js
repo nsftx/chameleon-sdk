cc11948d8221ac8f3456d93faf9509d46804fd80
Lana <dzidic.lana@nsoft.com>
2018-10-18 16:45:22 +0200

    Add runner detection utility chmjs/chameleon-bundle-team#9

--- src/utility/index.js
+++ src/utility/index.js
@@ -2,3 +2,4 @@ export { default as binding } from './binding';
 export { default as localStorage } from './localStorage';
 export { default as logger } from './logger';
 export { default as mapping } from './mapping';
+export { default as runnerDetection } from './runnerDetection';
---
+++ src/utility/runnerDetection.js
@@ -0,0 +1,30 @@
+/* eslint no-prototype-builtins: "off" */
+
+import { isNil } from 'lodash';
+import { logger } from '@/utility';
+
+const RUNNER_NAME = 'ChameleonRunner';
+
+export default {
+  /*
+  Detects is app running inside runner device.
+  Basic check is to check does ChameleonRunner global exist.
+  TODO: Write fallback detection
+  */
+  detectRunner() {
+    if (!window.hasOwnProperty(RUNNER_NAME)) {
+      logger.error('ChameleonRunner not detected.');
+      return false;
+    }
+
+    const runner = window[RUNNER_NAME];
+
+    if (isNil(runner)) {
+      logger.warning('ChameleonRunner detected but has nil value.');
+      return false;
+    }
+
+    logger.info('ChameleonRunner detected.');
+    return runner;
+  },
+};
