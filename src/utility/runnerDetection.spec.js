796f142742b8b260a37f1c99a5c29f5cb0dad0a9
Lana <dzidic.lana@nsoft.com>
2018-10-19 10:51:58 +0200

    Update tests

--- src/utility/runnerDetection.spec.js
+++ src/utility/runnerDetection.spec.js
@@ -6,13 +6,22 @@ describe('Runner detection utility', () => {
   });
 
   it('should detect global ChameleonRunner variable', () => {
-    global.ChameleonRunner = {};
+    global.ChameleonRunner = {
+      sendMessage: () => {},
+    };
 
     const runner = runnerDetection.detectRunner();
     expect(runner).toBeTruthy();
   });
 
   it('should return false when global ChameleonRunner variable is not defined', () => {
+    const runner = runnerDetection.detectRunner();
+    expect(runner).toBeFalsy();
+  });
+
+  it('should return false when global ChameleonRunner variable does not have sendMessage method', () => {
+    global.ChameleonRunner = {};
+
     const runner = runnerDetection.detectRunner();
     expect(runner).toBeFalsy();
   });
