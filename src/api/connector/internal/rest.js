1be1b078188e8018cd0a5fc52ceb12fa5d4a33ca
Lana <dzidic.lana@nsoft.com>
2018-10-30 16:15:55 +0100

    Prettify code attempt no.2 #28

--- src/api/connector/internal/rest.js
+++ src/api/connector/internal/rest.js
@@ -12,16 +12,27 @@ const getBaseBlueprintUrl = (connectorOptions, connectorType) => {
   return url;
 };
 
-// Attach necessary data for READ implementation
-const formatViewModel = (viewModel, dataPackageId) => {
-  const viewData = {
-    id: viewModel.id,
-    name: viewModel.name,
-    dataPackage: dataPackageId,
-    record: viewModel.rootRecordId,
-  };
+const getViewModels = (baseUrl, dataPackageId) => {
+  const latestSchemaUrl = `${baseUrl}/data-packages/${dataPackageId}/schema-versions/uncommitted`;
+  const viewModelsReq = http.get(latestSchemaUrl).then((result) => {
+    const { views } = result.data.schema;
 
-  return viewData;
+    // Attach necessary data for READ implementation
+    const viewModels = map(views, (view) => {
+      const viewData = {
+        id: view.id,
+        name: view.name,
+        dataPackage: dataPackageId,
+        record: view.rootRecordId,
+      };
+
+      return viewData;
+    });
+
+    return viewModels;
+  });
+
+  return viewModelsReq;
 };
 
 export default {
@@ -34,15 +45,9 @@ export default {
     // TODO: Change implementation after https://github.com/chmjs/ride-storage-blueprint/issues/40
     return http.get(`${baseUrl}/data-packages`).then((response) => {
       const dataPackage = response.data.dataPackages[0];
-      const latestSchemaUrl = `${baseUrl}/data-packages/${dataPackage.id}/schema-versions/uncommitted`;
 
       // Take first data package and fetch its latest schema
-      return http.get(latestSchemaUrl).then((result) => {
-        const { views } = result.data.schema;
-        const viewModels = map(views, view => formatViewModel(view, dataPackage.id));
-
-        return viewModels;
-      });
+      return getViewModels(baseUrl, dataPackage.id);
     });
   },
   getSourceData() {
