eaf6f21c78d6e5af6e31a299ae465bb53140dcbf
Ahmet Macić <macic.ahmet@nsoft.com>
2018-11-08 15:26:26 +0100

    Remove header comment
    #27

--- src/api/connector/external/rest.js
+++ src/api/connector/external/rest.js
@@ -1,10 +1,3 @@
-/*
-Generic HTTP REST connector.
-This connector should implement OPEN API specification.
-https://github.com/OAI/OpenAPI-Specification
-https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.1.md
-*/
-// import { getCommonMeta } from '../utility';
 import http from 'axios';
 import { toLower } from 'lodash';
 
