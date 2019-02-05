c51a9f9aa35a7d284e3be157ba40df699e852e6a
Snjezana Dreznjak <dreznjak.snjezana@nsoft.com>
2019-02-05 12:29:17 +0100

    Added errorable mixin and spec file testing errorHandler messages
    ref #chmjs/chameleon-vuetify/issues/170

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,6 +1,7 @@
 export { default as bindable } from './bindable';
 export { default as dependable } from './dependable';
 export { default as elementable } from './elementable';
+export { default as errorable } from './errorable';
 export { default as sourceable } from './sourceable';
 export { default as reactionable } from './reactionable';
 export { default as themable } from './themable';
