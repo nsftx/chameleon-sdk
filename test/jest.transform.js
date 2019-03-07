e9246326ebb09ae03d07e6330fac0748e0c5ddbd
Lana <dzidic.lana@nsoft.com>
2019-03-07 15:50:54 +0100

    Update babel & co

--- test/jest.transform.js
+++ test/jest.transform.js
@@ -3,12 +3,10 @@ We are using babel this way since babelrc or babel package
 settings are merged in projects using this lib.
 */
 module.exports = require('babel-jest').createTransformer({
-  presets: ['babel-preset-env'],
+  presets: ['@babel/preset-env'],
   plugins: [
-    ['transform-runtime', {
-      polyfill: false,
+    ['@babel/plugin-transform-runtime', {
       regenerator: true,
     }],
-    'transform-object-rest-spread',
   ],
 });
