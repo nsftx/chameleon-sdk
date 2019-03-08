5d8d2e7bc44184434c84b5fa0d30d279dad226a2
Lana <dzidic.lana@nsoft.com>
2019-03-08 15:26:36 +0100

    Add basic filtering for local connector #81

--- src/api/connector/internal/local.js
+++ src/api/connector/internal/local.js
@@ -5,8 +5,44 @@ schema and data over time.
 */
 
 import http from 'axios';
-import { map, pick } from 'lodash';
+import {
+  each,
+  filter,
+  flatMap,
+  map,
+  pick,
+} from 'lodash';
 import { getSavedSources } from '../common';
+
+const filterOperations = {
+  eq: (data, filterRule) => filter(data,
+    item => item[filterRule.fields[0]] === filterRule.values[0]),
+  ne: (data, filterRule) => filter(data,
+    item => item[filterRule.fields[0]] !== filterRule.values[0]),
+  in: () => {
+
+  },
+  notIn: () => {
+
+  },
+  startsWith: () => {
+
+  },
+};
+
+const filterData = (data, filters) => {
+  let result = data;
+
+  each(filters, (filterRule) => {
+    if (!filterRule.operator) return;
+
+    result = filterOperations[filterRule.operator](result, filterRule);
+  });
+
+  return result;
+};
+
+const flattenFiltersDefinition = definition => flatMap(definition, item => [item, ...item.and]);
 
 export default {
   getSources(connector, { savedOnly }) {
@@ -22,11 +58,14 @@ export default {
     const url = `${connector.type.options.endpoint}/${source.id}.json`;
     return http.get(url).then((response) => {
       const result = response.data;
+      const filters = flattenFiltersDefinition(source.filters);
+
+      const filteredData = filterData(result, filters);
       const columns = map(source.schema, n => n.name);
 
       return {
         [source.name]: {
-          items: map(result, m => pick(m, columns)),
+          items: map(filteredData, m => pick(m, columns)),
         },
       };
     });
