f4d8f142feb72da30644677cd52e28c55b1369d1
Vinko Soldo <soldo.vinko@nsoft.com>
2020-01-09 17:01:49 +0100

    Add unit test Ref #130

---
+++ src/utility/bundleInjector.spec.js
@@ -0,0 +1,45 @@
+import bundleInjector from './bundleInjector';
+
+describe('bundleInjector utility', () => {
+  it('promise', () => {
+    const bundle = {
+      name: 'material',
+      namespace: 'c-',
+      version: 156,
+      styles: [
+        'bundle.min.css',
+      ],
+      scripts: [
+        {
+          script: 'bundle.min.js',
+          type: 'components',
+          global: '__CHAMELEON_MATERIAL__',
+        },
+        {
+          script: 'bundle.meta.js',
+          type: 'meta',
+          global: '__CHAMELEON_MATERIAL_META__',
+        },
+      ],
+      helpPath: 'help',
+      plugins: {
+        vuetify: {
+          components: [
+            'VDataTable',
+            'VTextarea',
+            'VTreeview',
+            'VDatePicker',
+          ],
+        },
+      },
+    };
+    const config = {
+      scripts: ['components'],
+      baseURL: 'https://storage.googleapis.com/chameleon-storage',
+    };
+    return bundleInjector.loadBundles(bundle, config).then((data) => {
+      // eslint-disable-next-line
+      expect(data).toMatchObject(["__CHAMELEON_MATERIAL__"]);
+    });
+  });
+});
